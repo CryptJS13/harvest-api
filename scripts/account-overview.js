54a0e3927237ccf6e64e177622be3e034e1f9cc6
JasperSlimmens <jasper@slimmens.nl>
2021-10-18 14:32:24 +0200

    update

--- scripts/account-overview.js
+++ scripts/account-overview.js
@@ -2,7 +2,7 @@ const BigNumber = require('bignumber.js')
 const { UI_DATA_FILES } = require('../src/lib/constants')
 const { getUIData } = require('../src/lib/data')
 const keys = require('../dev-keys.json')
-const { getPoolBalances, getUsdValue, getRariBalance, getTokenBalance, getUSDEUR } = require('./utils')
+const { getNativeBalances, getPoolBalances, getUsdValue, getRariBalance, getTokenBalance, getUSDEUR } = require('./utils')
 const initDb = require('../src/lib/db')
 const { cliPreload } = require('../src/runtime/pollers')
 
@@ -12,7 +12,14 @@ const main = async () => {
   const tokens = await getUIData(UI_DATA_FILES.TOKENS)
   const pools = await getUIData(UI_DATA_FILES.POOLS)
   const accounts = keys.walletAddresses
+  // const accounts = ["0x814055779F8d2F591277b76C724b7AdC74fb82D9"]
   console.log("Accounts:", accounts)
+  const ethPrice = await getUsdValue(['WETH'], [1])
+  const farmPrice = await getUsdValue(['FARM'], [1])
+  const iFarmPrice = await getUsdValue(['IFARM'], [1])
+  console.log("ETH price:       ", ethPrice, "usd")
+  console.log("FARM price:      ", farmPrice, "usd")
+  console.log("iFARM price:     ", iFarmPrice, "usd")
 
   let allStaked = 0, allRewards = 0
   const usdEUR = await getUSDEUR()
@@ -21,6 +28,13 @@ const main = async () => {
     console.log("---------------------------------------------")
     console.log("Account:", account)
     console.log("")
+    console.log("Fetching native token balances...")
+    const nativeBalances = await getNativeBalances(account)
+
+    console.log("Total native balance:", nativeBalances, "usd")
+    console.log("")
+    allStaked += nativeBalances
+
     console.log("Fetching pools...")
     let totalUnderlying = 0, totalRewards = 0
     for (const j in pools) {
@@ -32,11 +46,11 @@ const main = async () => {
         console.log("Underlying value:  ", underlyingValue, "usd")
         rewardValue = await getUsdValue(result.rewardTokens, result.rewardAmounts)
         console.log("Reward value:      ", rewardValue, "usd")
-        console.log("")
         totalUnderlying += underlyingValue
         totalRewards += rewardValue
       }
     }
+    console.log("")
     console.log("Total staked:      ", totalUnderlying, "usd")
     console.log("Total rewards:     ", totalRewards, "usd")
     console.log("")
@@ -60,17 +74,17 @@ const main = async () => {
       if (result > 0) {
         console.log("Token", l, "of", Object.keys(tokens).length, ":", k)
         console.log("USD value:         ", result, "usd")
-        console.log("")
         totalBalance += result
       }
       l++
     }
+    console.log("")
     console.log("Total token balance:", totalBalance, "usd")
     console.log("")
     allStaked += totalBalance
 
     console.log(".............................................")
-    const allBalance = totalUnderlying+totalRewards+rariBalance.supplied-rariBalance.borrowed+totalBalance
+    const allBalance = nativeBalances+totalUnderlying+totalRewards+rariBalance.supplied-rariBalance.borrowed+totalBalance
     console.log("Account balance:     ", allBalance, "usd")
     console.log("Account balance:     ", allBalance/usdEUR, "eur")
   }
